50c3431e95bc3932eaedca90057a087b0bf84f2a
dobraeuyan <fernandolopezp119@gmail.com>
2025-08-26 20:44:08 -0600

    Last Marshmallow

--- Android.mk
+++ Android.mk
@@ -109,7 +109,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := lenovo
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
